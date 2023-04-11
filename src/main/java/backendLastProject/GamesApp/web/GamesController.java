54cb5af962134ffb73e6db464367b668da5320eb
Arina <arina.soldan@gmail.com>
2023-04-11 14:41:25 +0300

    Add new game-function added

--- src/main/java/backendLastProject/GamesApp/web/GamesController.java
+++ src/main/java/backendLastProject/GamesApp/web/GamesController.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import backendLastProject.GamesApp.domain.Game;
 import backendLastProject.GamesApp.domain.GameRepository;
 import backendLastProject.GamesApp.domain.GenreRepository;
+import backendLastProject.GamesApp.domain.Publisher;
 import backendLastProject.GamesApp.domain.PublisherRepository;
 
 
@@ -32,5 +34,20 @@ public class GamesController {
 		return "gamelist";
 	}
 	
+	@RequestMapping(value = "/add")
+	public String addGame(Model model){
+	   model.addAttribute("game", new Game());
+	   model.addAttribute("genres", genreRepo.findAll());
+	   return "addgame";
+	}
+
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String saveGame(Game game){
+	    Publisher publisher = game.getPublisher();
+	    publisherRepo.save(publisher); // save the publisher entity first
+	    gameRepo.save(game); // save the game entity
+	    return "redirect:gamelist";
+	}
 
 }
