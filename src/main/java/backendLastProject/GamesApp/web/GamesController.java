78c1830f896063bc9807211310e1b985205f1bb2
Arina <arina.soldan@gmail.com>
2023-04-11 15:18:43 +0300

    Edit game-function added

--- src/main/java/backendLastProject/GamesApp/web/GamesController.java
+++ src/main/java/backendLastProject/GamesApp/web/GamesController.java
@@ -1,16 +1,19 @@
 package backendLastProject.GamesApp.web;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import backendLastProject.GamesApp.domain.Game;
 import backendLastProject.GamesApp.domain.GameRepository;
+import backendLastProject.GamesApp.domain.Genre;
 import backendLastProject.GamesApp.domain.GenreRepository;
 import backendLastProject.GamesApp.domain.Publisher;
 import backendLastProject.GamesApp.domain.PublisherRepository;
@@ -49,5 +52,23 @@ public class GamesController {
 	    gameRepo.save(game); // save the game entity
 	    return "redirect:gamelist";
 	}
+	
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+	public String editGame(@PathVariable("id") Long gameId, Model model){
+	    Optional<Game> gameOptional = gameRepo.findById(gameId);
+	    if (gameOptional.isPresent()) {
+	        Game game = gameOptional.get();
+	        List<Genre> genres = (List<Genre>) genreRepo.findAll();
+	        Publisher publisher = game.getPublisher();
+	        model.addAttribute("game", game);
+	        model.addAttribute("genres", genres);
+	        model.addAttribute("publisher", publisher);
+	        return "editgame";
+	    } else {
+	        // handle the case where the requested game is not found
+	        return "error";
+	    }
+	}
+
 
 }
